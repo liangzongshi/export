2004464df25486a79283dd967ec3fee70f85e2a7
liangzongshi <liangzongshi.20@gmail.com>
2020-07-14 20:15:37 +0700

    ss

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ const ObjectsToCsv = require('objects-to-csv')
 
 const writeLog = async () => {
     const orders = (await db.system({}, 'orders'))[0].orders
-    const logs = orders.slice(105, 10105)
-    console.log(logs.length)
-    const csv = new ObjectsToCsv(logs)
+    const logs = orders.slice(105, 5105)
+    var re_logs = []
+    for ( var i = 0; i < logs.length; i++){
+        console.log(i)
+        re_logs.push({
+            Timestamp: logs[i].timestamp,
+            Type: logs[i].type,
+            Amount: logs[i].amount,
+            'Exchange Buy': logs[i].buy.exchanger,
+            'Price Buy': logs[i].buy.price,
+            'Exchange Sell': logs[i].sell.exchanger,
+            'Price Sell': logs[i].sell.price,
+            Profit: logs[i].profit
+        })
+    }
+
+    const csv = new ObjectsToCsv(re_logs)
     await csv.toDisk(`./finfine.csv`, { bom: true })
     console.log('Write Log HFT')
 }
