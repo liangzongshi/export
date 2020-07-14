3e239f607cfbdf2087b48e0c087894d2c6a5395c
liangzongshi <liangzongshi.20@gmail.com>
2020-07-14 19:17:46 +0700

    ss

---
+++ app.js
@@ -0,0 +1,13 @@
+const DB = require('./db')
+const db = new DB()
+const ObjectsToCsv = require('objects-to-csv')
+
+const writeLog = async () => {
+    const orders = (await db.system({}, 'orders'))[0].orders
+    const logs = orders.slice(105, orders.length)
+    const csv = new ObjectsToCsv(logs)
+    await csv.toDisk(`./finfine.csv`, { bom: true })
+    console.log('Write Log HFT')
+}
+
+writeLog()
